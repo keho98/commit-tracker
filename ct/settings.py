eec8e72f3677653ea01a255cd4296eac8700d86c
Kevin Ho <keho98@gmail.com>
2013-04-10 11:38:32 -0700

    Playing with settings

--- ct/settings.py
+++ ct/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'ct-dev',                      # Or path to database file if using sqlite3.
+        'NAME': 'ct_dev',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'keho98',
         'PASSWORD': 'ct-dev-keho98',
