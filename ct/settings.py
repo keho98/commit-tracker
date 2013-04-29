67190c7974e1af63b1b03732c9f5a7784604b8e7
Kevin Ho <keho98@gmail.com>
2013-04-28 23:32:17 -0700

    Fixing up app settings

--- ct/settings.py
+++ ct/settings.py
@@ -124,7 +124,8 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-)
+    'commit_tracker'
+    )
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
