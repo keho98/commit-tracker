7f4a2c9cb079f497a739bc5293a674eb321806bb
Kevin Ho <keho98@gmail.com>
2013-09-22 23:56:03 -0700

    Working on fixing up sunburnt interface

--- solr_count/solr.py
+++ solr_count/solr.py
@@ -4,3 +4,16 @@ import httplib2
 SOLR_URL = "http://localhost:8983/solr"
 h = httplib2.Http(cache="/var/tmp/solr_cache")
 solr_interface = sunburnt.SolrInterface(url=SOLR_URL, http_connection=h)
+
+document = {"id":"0553573403",
+            "cat":"book",
+            "name":"A Game of Thrones",
+            "price":7.99,
+            "inStock": True,
+            "author_t":
+            "George R.R. Martin",
+            "series_t":"A Song of Ice and Fire",
+            "sequence_i":1,
+            "genre_s":"fantasy"}
+
+solr_interface.add(document)
